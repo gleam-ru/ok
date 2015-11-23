97f012d9806b37e4fc1a4da430b545b9e67ba971
gleam-ru <roman.demeshenko@gmail.com>
2015-11-23 14:05:36 +0300

    vue test

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -12,7 +12,24 @@ module.exports = {
             pageTitle: 'Index',
             test: 'test_data',
         }
-        return res.render('index', data);
+        Q()
+            .then(function() {
+                return User
+                    .find()
+                    .populateAll()
+            })
+            .then(function(users) {
+                data.users = users;
+                return Role
+                    .find()
+            })
+            .then(function(roles) {
+                data.roles = roles;
+            })
+            .then(function() {
+                return res.render('index', data);
+            })
+            .catch(res.serverError)
     },
 
 };
--- assets/ViewControllers/index.js
+++ assets/ViewControllers/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    console.log(globalVars.test_var)
+    new Vue({
+        el: '#vue',
+        template: Jade.Vue.usersTable(),
+        data: {
+            users: _.map(globalVars.users, function(user) {
+                user.roles = _.map(user.roles, 'id');
+                return user;
+            }),
+            roles: globalVars.roles,
+        },
+    })
 });
