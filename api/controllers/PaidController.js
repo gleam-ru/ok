4ec8587ab444bf7dcafefb88a1d63896fa233e48
gleam-ru <roman.demeshenko@gmail.com>
2015-11-26 16:40:29 +0300

    paid/ideas/idea

--- api/controllers/PaidController.js
+++ api/controllers/PaidController.js
@@ -82,5 +82,48 @@ module.exports = {
             })
     },
 
+
+
+    idea: function(req, res) {
+        var id = parseInt(req.param('id'));
+        if (!id) {
+            return res.notFound();
+        }
+        var data = {
+            pageTitle: 'Ideas',
+            title: 'Ideas',
+            bc: [
+                {name: 'Home', href: '/'},
+                {name: 'Paid', href: '/paid'},
+                {name: 'Ideas', href: '/paid/ideas'},
+                {name: 'Idea_'+id, href: '/paid/idea/get/1'},
+            ],
+
+            idea: {
+                description: '',
+            },
+        }
+        data.idea.description += "<h2 class='mb-10'>Добрый день.</h2>"
+        data.idea.description += "<p>"
+        data.idea.description +=     "Сегодня хотим обратить Ваше внимание на акции Уралкалия, которые торгуются с 30%-м дисконтом к аналогам. Этот дисконт связан с недавним снижением котировок на фоне новостей об исключении акций Уралкалия из индекса MSCI."
+        data.idea.description += "<img src='/images/paid/ideas/1/unnamed.gif' style='height: 330px; margin: 20px 0px; display: block;' />"
+        data.idea.description += "</p>"
+        data.idea.description += "<p>"
+        data.idea.description +=     "На наш взгляд, новость уже отыграна рынком и дальнейших продаж, в связи с исключением из MSCI, не будет. Это позволит инвесторам сосредоточить свое внимание на фундаментальных показателях компании. Также стоит отметить, что слухи об обратном выкупе акций до сих пор официально не подтвердились. Однако консенсус прогноз аналитиков таков, что если выкуп акций все-таки будет, то цена выкупа будет около 16$, что существенно выше рынка. "
+        data.idea.description += "</p>"
+        data.idea.description += "<p>"
+        data.idea.description +=     "С технической точки зрения акции консолидируются у горизонтального уровня 153 рубля, что является хорошим моментом для покупки."
+        data.idea.description += "</p>"
+        data.idea.description += "<p>"
+        data.idea.description +=     "Рекомендуем купить акции Уралкалия не выше 157 рублей."
+        data.idea.description += "<br />"
+        data.idea.description +=     "Цель: 180 рублей (<span class='percent_green'>+14.6%</span>)."
+        data.idea.description += "</p>"
+        return Q()
+            .then(function() {
+                return res.render('paid/idea', data)
+            })
+    },
+
 };
 
