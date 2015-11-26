fd54f797963ad9bea87bc4bd5eec437cb35b053b
gleam-ru <roman.demeshenko@gmail.com>
2015-11-26 16:12:38 +0300

    paid/ideas

--- api/controllers/PaidController.js
+++ api/controllers/PaidController.js
@@ -40,5 +40,47 @@ module.exports = {
             })
     },
 
+
+
+    ideas: function(req, res) {
+        var data = {
+            pageTitle: 'Ideas',
+            title: 'Ideas',
+            bc: [
+                {name: 'Home', href: '/'},
+                {name: 'Paid', href: '/paid'},
+                {name: 'Ideas', href: '/paid/ideas'},
+            ],
+
+            ideas: [
+                {
+                    title: 'URKA',
+                    url: '/paid/ideas/get/1',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'SBERP',
+                    url: '/paid/ideas/get/1',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'GAZP',
+                    url: '/paid/ideas/get/1',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'LKOH',
+                    url: '/paid/ideas/get/1',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'MTLR',
+                    url: '/paid/ideas/get/1',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }
+            ],
+        }
+        return Q()
+            .then(function() {
+                return res.render('paid/ideas', data)
+            })
+    },
+
 };
 
