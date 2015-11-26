19fd7b4ce04c6b66a51c505a5591e4f3eac4eee9
gleam-ru <roman.demeshenko@gmail.com>
2015-11-26 16:04:06 +0300

    закрытый раздел, индекс

--- api/controllers/PaidController.js
+++ api/controllers/PaidController.js
@@ -6,6 +6,39 @@
  */
 
 module.exports = {
-	
+    index: function(req, res) {
+        var data = {
+            pageTitle: 'Paid',
+            title: 'Paid',
+            bc: [
+                {name: 'Home', href: '/'},
+                {name: 'Paid', href: '/paid'},
+            ],
+
+            tiles: [
+                {
+                    title: 'Assets allocation',
+                    url: '/paid/allocation',
+                    image: 'pie.jpg',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'Active ideas',
+                    url: '/paid/ideas',
+                    image: 'ideas.jpg',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }, {
+                    title: 'Archive',
+                    url: '/paid/archive',
+                    image: 'archive.jpg',
+                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+                }
+            ],
+        }
+        return Q()
+            .then(function() {
+                return res.render('paid', data)
+            })
+    },
+
 };
 
