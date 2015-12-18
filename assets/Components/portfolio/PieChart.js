2e8f47cad15213353817fe9c96b36dd08835f67f
gleam-ru <roman.demeshenko@gmail.com>
2015-12-18 13:49:54 +0300

    PieChart (base highchart implementation)

--- assets/Components/portfolio/PieChart.js
+++ assets/Components/portfolio/PieChart.js
@@ -1,12 +1,15 @@
 module.exports = function(resolve) {
     System.importAll({
+        Highcharts: '/bower_components/highcharts/lib/highcharts.js',
         _raw: [
+
         ]
     })
-    .then(function() {
+    .then(function(imported) {
+        var Highcharts = imported.Highcharts;
         return {
             template: [
-                '<div style="min-height: 300px; border: solid 1px #ccc;">',
+                '<div id="fn-my-chart" style="min-height: 300px; border: solid 1px #ccc;">',
                 '</div',
             ].join(' '),
             props: [],
@@ -14,6 +17,29 @@ module.exports = function(resolve) {
             },
             ready: function() {
                 var vm = this;
+                $('#fn-my-chart').highcharts({
+                    chart: {
+                        type: 'bar'
+                    },
+                    title: {
+                        text: 'Fruit Consumption'
+                    },
+                    xAxis: {
+                        categories: ['Apples', 'Bananas', 'Oranges']
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Fruit eaten'
+                        }
+                    },
+                    series: [{
+                        name: 'Jane',
+                        data: [1, 0, 4]
+                    }, {
+                        name: 'John',
+                        data: [5, 7, 3]
+                    }]
+                });
             }
         }
     })
--- tasks/config/wiredep.js
+++ tasks/config/wiredep.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
         task: {
             ignorePath: "../../..",
             exclude: [
+                'highcharts',
                 'Chart.js',
                 'bootstrap-treeview.*',
                 'bootstrap-tagsinput.*',
