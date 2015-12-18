1f89242407a0a27cb98f3f49bfb94193136c3b11
gleam-ru <roman.demeshenko@gmail.com>
2015-12-18 13:31:19 +0300

    AssetsGroup multiple

--- assets/Components/portfolio/AssetsGroup.js
+++ assets/Components/portfolio/AssetsGroup.js
@@ -14,24 +14,25 @@ module.exports = function(resolve) {
                     '<input class="col-md-3" class="row" v-model="name">',
                     '<div class="col-md-2" @click="drop">'+Jade.els.iconButton('fa-trash')+'</div>',
                 '</div>',
-                '<kv-row style="margin-left: 20px;" v-for="row in shares"',
+                '<kv-row style="margin-left: 20px;" v-for="row in tickers"',
                     ':k.sync="row.k"',
                     ':v.sync="row.v"',
                     '@drop="droprow($index)"',
                     '>',
                 '</kv-row>',
                 '<div style="margin-left: 20px;" class="row" @click="addrow">'+Jade.els.iconButton('fa-plus')+'</div>',
+                Jade.els.separator(),
             ].join(' '),
-            props: ['name', 'shares',],
+            props: ['name', 'tickers',],
             methods: {
                 drop: function() {
                     this.$emit('drop');
                 },
                 droprow: function(index) {
-                    this.shares.splice(index, 1);
+                    this.tickers.splice(index, 1);
                 },
                 addrow: function() {
-                    this.shares.push({
+                    this.tickers.push({
                         k: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
                         v: parseInt(Math.random() * 100),
                     });
--- assets/ViewControllers/portfolio/edit.js
+++ assets/ViewControllers/portfolio/edit.js
@@ -11,23 +11,44 @@ $(document).ready(function() {
                 'assets-group': imported.ag,
             },
             template: [
-                '<assets-group',
-                    ':name.sync="name"',
-                    ':shares.sync="shares"',
-                    // '@drop="droprow($index)"',
+                '<assets-group v-for="ag in assets"',
+                    ':name.sync="ag.name"',
+                    ':tickers.sync="ag.tickers"',
+                    '@drop="dropassets($index)"',
                     '>',
                 '</assets-group>',
-                // '<div class="row" @click="addrow">'+Jade.els.iconButton('fa-plus')+'</div>',
+                '<div class="row" @click="addassets">'+Jade.els.iconButton('fa-plus')+'</div>',
             ].join(' '),
             data: {
-                shares: [
-                    {k: 'gazp', v: 138},
-                    {k: 'rasp', v: 105},
-                    {k: 'mltr', v: 130},
+                assets: [
+                    {
+                        name: 'Bonds',
+                        tickers: [
+                            {k: 'bond_1', v: 138},
+                            {k: 'bond_2', v: 105},
+                            {k: 'bond_3', v: 130},
+                        ],
+                    }, {
+                        name: 'Shares',
+                        tickers: [
+                            {k: 'gazp', v: 138},
+                            {k: 'rasp', v: 105},
+                            {k: 'mltr', v: 130},
+                        ],
+                    }
                 ],
-                name: 'Shares',
+
             },
             methods: {
+                dropassets: function(idx) {
+                    this.assets.splice(idx, 1);
+                },
+                addassets: function() {
+                    this.assets.push({
+                        name: '',
+                        tickers: [],
+                    });
+                },
             },
             ready: function() {
                 var vm = this;
