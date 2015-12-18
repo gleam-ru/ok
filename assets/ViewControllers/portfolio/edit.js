6927b3eb3feaaaffc6f2eda0130bef4883163d9c
gleam-ru <roman.demeshenko@gmail.com>
2015-12-18 13:09:37 +0300

    multirows

--- assets/ViewControllers/portfolio/edit.js
+++ assets/ViewControllers/portfolio/edit.js
@@ -11,15 +11,31 @@ $(document).ready(function() {
                 'kv-row': imported.row,
             },
             template: [
-                '<kv-row',
-                    ':k.sync="k"',
-                    ':v.sync="v"',
+                '<kv-row v-for="row in rows"',
+                    ':k.sync="row.k"',
+                    ':v.sync="row.v"',
+                    '@drop="droprow($index)"',
                     '>',
                 '</kv-row>',
+                '<div class="row" @click="addrow">'+Jade.els.iconButton('fa-plus')+'</div>',
             ].join(' '),
             data: {
-                k: 'gazp',
-                v: '138',
+                rows: [
+                    {k: 'gazp', v: 138},
+                    {k: 'rasp', v: 105},
+                    {k: 'mltr', v: 130},
+                ],
+            },
+            methods: {
+                droprow: function(index) {
+                    this.rows.splice(index, 1);
+                },
+                addrow: function() {
+                    this.rows.push({
+                        k: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+                        v: parseInt(Math.random() * 100),
+                    });
+                },
             },
             ready: function() {
                 var vm = this;
